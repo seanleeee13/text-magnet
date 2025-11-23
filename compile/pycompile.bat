083be27c0b2f9e5018cf456372b26a99ca846d62
seanleeee13 <seanleeee13@gmail.com>
2025-11-23 14:23:43 +0900

    imsi2:db-on-github-pgs

--- compile/pycompile.bat
+++ compile/pycompile.bat
@@ -1,1 +1,3 @@
-python3.12 -m nuitka --standalone --onefile --include-package=PIL --include-package=keyboard --enable-plugin=tk-inter --include-data-dir=data=data --include-data-dir=img=img --include-data-files=*.dll=./ --include-data-files=*.pyd=./ --output-dir=publish main.py
+cd ..
+cd text-magnet
+python3.12 -m nuitka --standalone --onefile --include-package=PIL --include-package=keyboard --enable-plugin=tk-inter --include-data-dir=data=data --include-data-dir=img=img --include-data-dir=db=db --output-dir=publish main.py
